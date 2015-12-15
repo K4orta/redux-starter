33485a2ae9517ccfe8cd535b0627058abb296219
Erik Wong <ewong@blizzard.com>
2015-12-15 08:42:43 -0800

    Add base styles and an async action

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,7 +1,35 @@
+import fetch from 'isomorphic-fetch';
+
 export const ACTION = 'ACTION';
 export function action(msg) {
   return {
     type: ACTION,
     msg: msg
+  };
+}
+
+export const REQUEST_ASYNC = 'REQUEST_ASYNC';
+function requestAsync() {
+  return {
+    type: REQUEST_ASYNC
+  };
+}
+
+export const RECEIVE_ASYNC = 'RECEIVE_ASYNC';
+function receiveAsync(json) {
+  return {
+    type: RECEIVE_ASYNC,
+    data: json
+  };
+}
+
+export function fetchAsync() {
+  return dispatch => {
+    dispatch(requestAsync());
+    return fetch(``)
+      .then(resp => resp.json())
+      .then(json => {
+        dispatch(receiveAsync(json));
+      });
   };
 }
--- src/components/component.js
+++ src/components/component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+require('../../styles/main');
 
 export default class Component extends React.Component {
   render() {
