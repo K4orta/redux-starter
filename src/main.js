720e79e2b0b078271289ea78cba8b0bb3dc92e68
Erik Wong <ewong@blizzard.com>
2016-01-06 09:58:16 -0800

    Update React Router

--- src/main.js
+++ src/main.js
@@ -2,19 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Connector from './containers/connector';
 import {Provider} from 'react-redux';
-import {createHistory} from 'history';
-import {Router, Route} from 'react-router';
+import {Router, Route, browserHistory} from 'react-router';
 import {syncReduxAndRouter} from 'redux-simple-router';
 
 import configStore from './store/configure-store';
 
 const store = configStore();
-const history = createHistory();
-syncReduxAndRouter(history, store);
+syncReduxAndRouter(browserHistory, store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
+    <Router history={browserHistory}>
       <Route path="/" component={Connector}>
       </Route>
     </Router>
